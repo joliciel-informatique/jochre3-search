5d1438877a76a13609a53eb13ad25146305e1490
Assaf Urieli <assaf.urieli@gmail.com>
2024-12-05 17:04:59 +0100

    Add error codes for frontend translations

--- modules/core/src/main/scala/com/joliciel/jochre/search/core/errors.scala
+++ modules/core/src/main/scala/com/joliciel/jochre/search/core/errors.scala
@@ -2,27 +2,31 @@ package com.joliciel.jochre.search.core
 
 import com.joliciel.jochre.search.core.service.MetadataCorrectionId
 
-class NotFoundException(message: String) extends Exception(message)
-class BadRequestException(message: String) extends Exception(message)
-class ConflictException(message: String) extends Exception(message)
+class NotFoundException(val code: String, message: String) extends Exception(message)
+class BadRequestException(val code: String, message: String) extends Exception(message)
+class ConflictException(val code: String, message: String) extends Exception(message)
 
 class DocumentAlreadyInIndexException(docRef: DocReference)
     extends ConflictException(
+      "DocumentAlreadyInIndex",
       f"The document ${docRef.ref} is already in the index. Delete it first or update the Alto layer only."
     )
 class DocumentNotFoundInIndexException(docRef: DocReference)
     extends NotFoundException(
+      "DocumentNotFoundInIndex",
       f"Document ${docRef.ref} not found in index"
     )
 
-class UnknownIndexFieldException(field: String) extends BadRequestException(f"Unknown IndexField: $field")
-class WrongFieldTypeException(message: String) extends BadRequestException(message)
-class NoSearchCriteriaException extends BadRequestException("No search criteria")
+class UnknownIndexFieldException(field: String)
+    extends BadRequestException("UnknownIndexField", f"Unknown IndexField: $field")
+class WrongFieldTypeException(message: String) extends BadRequestException("WrongFieldType", message)
+class NoSearchCriteriaException extends BadRequestException("NoSearchCriteria", "No search criteria")
 class PreferenceNotFound(username: String, key: String)
-    extends NotFoundException(f"Preference not found for user $username, key $key")
-class UnknownMetadataFieldException(field: String) extends BadRequestException(f"Unknown MetadataField: $field")
+    extends NotFoundException("PreferenceNotFound", f"Preference not found for user $username, key $key")
+class UnknownMetadataFieldException(field: String)
+    extends BadRequestException("UnknownMetadataField", f"Unknown MetadataField: $field")
 class UnknownMetadataCorrectionIdException(id: MetadataCorrectionId)
-    extends NotFoundException(f"Unknown metadata correction: $id")
+    extends NotFoundException("UnknownMetadataCorrectionId", f"Unknown metadata correction: $id")
 
 class PageNotFoundException(docRef: DocReference, pageNumber: Int)
     extends Exception(f"In document ${docRef.ref}, page $pageNumber not found in database")
@@ -31,4 +35,5 @@ class RowNotFoundException(docRef: DocReference, pageNumber: Int, rowIndex: Int)
 class ImageFileNotFoundException(docRef: DocReference, pageNumber: Int)
     extends Exception(f"No image found for document ${docRef.ref}, page $pageNumber")
 
-class UnparsableQueryException(message: String) extends BadRequestException(f"Unparsable query: $message")
+class UnparsableQueryException(message: String)
+    extends BadRequestException("UnparsableQuery", f"Unparsable query: $message")
--- modules/core/src/main/scala/com/joliciel/jochre/search/core/service/errors.scala
+++ modules/core/src/main/scala/com/joliciel/jochre/search/core/service/errors.scala
@@ -1,14 +1,14 @@
 package com.joliciel.jochre.search.core.service
 
-import com.joliciel.jochre.search.core.{BadRequestException, DocReference, NotFoundException}
+import com.joliciel.jochre.search.core.{BadRequestException, DocReference}
 
-class BadPdfFileFormat(message: String) extends BadRequestException(message)
-class BadAltoFileFormat(message: String) extends BadRequestException(message)
-class BadImageZipFileFormat(message: String) extends BadRequestException(message)
-class BadMetadataFileFormat(message: String) extends BadRequestException(message)
-class BadOffsetForImageSnippet(message: String) extends BadRequestException(message)
-class IndexFieldNotAggregatable(message: String) extends BadRequestException(message)
-class NoFieldRequestedForAggregation extends BadRequestException("No fields were selected for aggregation")
-class InvalidJsonException(message: String) extends BadRequestException(message)
+class BadPdfFileFormat(message: String) extends BadRequestException("BadPdfFileFormat", message)
+class BadAltoFileFormat(message: String) extends BadRequestException("BadAltoFileFormat", message)
+class BadImageZipFileFormat(message: String) extends BadRequestException("BadImageZipFileFormat", message)
+class BadMetadataFileFormat(message: String) extends BadRequestException("BadMetadataFileFormat", message)
+class BadOffsetForImageSnippet(message: String) extends BadRequestException("BadOffsetForImageSnippet", message)
+class IndexFieldNotAggregatable(message: String) extends BadRequestException("IndexFieldNotAggregatable", message)
+class NoFieldRequestedForAggregation
+    extends BadRequestException("NoFieldRequestedForAggregation", "No fields were selected for aggregation")
 class WordOffsetNotFound(docRef: DocReference, wordOffset: Int)
-    extends BadRequestException(f"No word found for document ${docRef.ref}, offset $wordOffset")
+    extends BadRequestException("WordOffsetNotFound", f"No word found for document ${docRef.ref}, offset $wordOffset")
